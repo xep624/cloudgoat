f07569d2d9ab1c6ec8970856e6cb8ab20f222e78
Pawel Rzepa <pawel.rzepa@outlook.com>
2019-12-16 19:54:12 +0100

    Update extract_creds.py

--- extract_creds.py
+++ extract_creds.py
@@ -7,8 +7,6 @@ with open('./terraform/terraform.tfstate') as tfstate:
   data = json.load(tfstate)
 
   with open('./credentials.txt', 'w') as credfile:
-    credfile.write("Bob's Access Key:         " + data['modules'][0]['resources']['aws_iam_access_key.bob_key']['primary']['id'] + "\n")
-    credfile.write("Bob's Secret Key:         " + data['modules'][0]['resources']['aws_iam_access_key.bob_key']['primary']['attributes']['secret'] + "\n" )
-    credfile.write("Joe's Access Key:         " + data['modules'][0]['resources']['aws_iam_access_key.joe_key']['primary']['id'] + "\n")
-    credfile.write("Joe's Secret Key:         " + data['modules'][0]['resources']['aws_iam_access_key.joe_key']['primary']['attributes']['secret'] + "\n" )
+    credfile.write("Bob's Access Key:         " + data['resources'][0]['instances'][0]['attributes']['id'] + "\n")
+    credfile.write("Bob's Secret Key:         " + data['resources'][0]['instances'][0]['attributes']['secret'] + "\n")
 
